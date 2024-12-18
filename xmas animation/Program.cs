fb62cc30b7be7b40e9626b28edf6038497cab964
Liam Bolan <AQ231688@aquinas.ac.uk>
2024-12-18 10:10:56 +0000

    ea

--- xmas animation/Program.cs
+++ xmas animation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Data;
 
 namespace xmas_animation
 {
@@ -60,46 +61,51 @@ namespace xmas_animation
 
             //-----------------------------------------------------------------------------------------------------------
             // 1. Create variables to store height, width, framecount and framerate
-            double height = 0; double width = 0; double framecount = 0; int framerate = 0;
+
+            int height = 0;
+            int width = 0;
+            int framecount = 0;
+            int framerate = 0;
+
             // 2. create a streamreader to access the xamsanimation.txt file
-            StreamReader xmasanimation = new StreamReader ("Xmasanimation.txt");
+
+            StreamReader xmasanimation = new StreamReader("xmasanimation.txt");
+
             //3. Read in the first four lines of the text file and get values for the variables you created in 1.
 
-            height = double.Parse(xmasanimation.ReadLine());
-            width = double.Parse(xmasanimation.ReadLine());
-            framecount = double.Parse(xmasanimation.ReadLine());
-            framerate = int.Parse(xmasanimation.ReadLine());
+            xmasanimation.ReadLine();
+            xmasanimation.ReadLine();
+            xmasanimation.ReadLine();
+            xmasanimation.ReadLine();
 
             //4. Create a for loop to read each frame in the animation file
 
-            for(int i = 0; i < framecount; i++)
+            for (int i = 0; i < framecount; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (int j = 0; j < width; j++)
                 {
-                    string line = xmasanimation.ReadLine();
-                    for (int k = 0; k < width; k++)
-                    {
-                        int pixel = HexToDecimal(line[k]);
-                        Console.BackgroundColor = (ConsoleColor)pixel;
-                        Console.Write(" ");
-                    }
-                    Console.WriteLine();
+                    string row = xmasanimation.ReadLine();
+                    
                 }
-                System.Threading.Thread.Sleep(1000 / framerate);
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.Clear();
             }
-            xmasanimation.Close();
-
             
-            //9. Set console color using :-console.backgroundcolor=(ConsoleColor)pixelvalue
-            // write a space onto the console.
+          
+                //5. Create a nested for loop inside the loop in 4. to process each row in a frame
+             
+                      //6. read a row of data in the loop into a string variable
+                   
+                      // 7. create another nested FOR loop inside the loop in 5. to process each pixel in each row.
+               
+                            //8. Convert each character in the string to its decimal value :- use the function HextoDecimal
+                      
+                            //9. Set console color using :-console.backgroundcolor=(ConsoleColor)pixelvalue
+                            // write a space onto the console.
+                   
+                     // 10. writeline - start a new row after the inner loop has finished
+          
+                 //11. Pause between frames after the second loop using :- System.Threading.Thread.Sleep(1000/framerate)
 
-            // 10. writeline - start a new row after the inner loop has finished
-
-            //11. Pause between frames after the second loop using :- System.Threading.Thread.Sleep(1000/framerate)
-
-            //12.After the pause set the background color to black and clear the screen
+                //12.After the pause set the background color to black and clear the screen
 
 
             //13. close the stream reader after all the loops have finished
